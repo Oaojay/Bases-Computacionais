3c7ef6fe9fb877983d6d3f8c52a1fd4263010650
João Pedro Alencar <51497206+Oaojay@users.noreply.github.com>
2019-07-10 23:30:37 -0300

    Update Tarefa3JoaoPedroAraujodeAlencar.py

--- Tarefa3JoaoPedroAraujodeAlencar.py
+++ Tarefa3JoaoPedroAraujodeAlencar.py
@@ -13,14 +13,14 @@ h=1.5 #define a variavel das horas para transformar em segundos
 
 print('o total de segundos é',s*h,'s') #imprime o resultado em segundos
 
-s=3600#define a variavel dos segundos
+s=3600 #define a variavel dos segundos
 
-h=12#define a variavel das horas para transformar em segundos
+h=12 #define a variavel das horas para transformar em segundos
 
-print('o total de segundos é',s*h,'s')#imprime o resultado em segundos
+print('o total de segundos é',s*h,'s') #imprime o resultado em segundos
 
-s=3600#define a variavel dos segundos
+s=3600 #define a variavel dos segundos
 
-h=24#define a variavel das horas para transformar em segundos
+h=24 #define a variavel das horas para transformar em segundos
 
-print('o total de segundos é',s*h,'s')#imprime o resultado em segundos
+print('o total de segundos é',s*h,'s') #imprime o resultado em segundos
