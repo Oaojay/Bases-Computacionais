86fc94d961ec490faaa12926ae8df4c0d970f933
João Pedro Alencar <51497206+Oaojay@users.noreply.github.com>
2019-07-12 20:37:16 -0300

    Add files via upload

---
+++ Tarefa7JoaoPedroAraujodeAlencar.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+Este é um arquivo de script temporário.
+"""
+import numpy as np
+    
+x=1 #Define a variavel a ser verificada se é par ou ímpar
+z=x%2 #Define a variavel com a operação para verificar se o número é par ou impar 
+if z == 0: #Define uma condição para a variavel
+    print ("x par") #Imprime caso a condição if seja verdadeira
+else: #Define uma condição para a variavel
+    print ("x é ímpar") #Imprime caso a condição else seja verdadeira
+    
+x=24 #Define a variavel a ser verificada se é ímpar ou par
+z=x%2
+if z == 0:
+    print ("x par")
+else:
+    print ("x é ímpar")
+    
+x=10 #Define a variavel a ser verificada se é ímpar ou par
+z=x%2
+if z == 0:
+    print ("x par")
+else:
+    print ("x é ímpar")
+      
+x=5 #Define a variavel a ser verificada se é ímpar ou par
+z=x%2
+if z == 0:
+    print ("x par")
+else:
+    print ("x é ímpar")
