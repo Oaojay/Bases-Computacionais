e1c3b03a57d97700f2fd1602ac7fdad9a13b96ee
João Pedro Alencar <51497206+Oaojay@users.noreply.github.com>
2019-08-09 18:13:13 -0300

    Add files via upload

---
+++ Tarefa12JoaoPedroAraujodeALencar.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+Este é um arquivo de script temporário.
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+inflacao = pd.read_csv('inflacao.csv')
+
+A = 50
+B = 4
+for i in range(0, N+1, 1):
+    for i in i% != 0
+    B = B + 1/2**i
+print('S = ',S)
+
+C = 50
+D = 4
+for i in range(0, N+1, 1):
+    for i in i% != 0
+    D = D + 1/2**i
+print('S = ',S)
+
+E = 50
+F = 4
+for i in range(0, N+1, 1):
+    for i in i% != 0:
+    F = F + 1/2**i
+print('S = ',S)
+
+
+x = inflacao['Ano']
+y = inflacao['Inflação']
+z = np.ones((904))
+N = 30
+S = 0
+for i in range(0, N+1, 1):
+    S = S + 1/2**i
+print('S = ',S)
+
+
+print(x)
+
+
+print(y)
+        
